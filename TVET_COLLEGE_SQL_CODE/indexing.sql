094e5fb3f435ae08a80a30521619519c128ae21a
Abdisalam Hassan <bscnrb595022@spu.ac.ke>
2024-03-21 14:12:12 +0300

    indexing of the database

---
+++ TVET_COLLEGE_SQL_CODE/indexing.sql
@@ -0,0 +1,5 @@
+CREATE INDEX users_email_idx ON users (email);
+CREATE INDEX locations_address_idx ON locations (address);
+CREATE INDEX products_name_idx ON products (name);
+CREATE INDEX products_category_idx ON products (category);
+CREATE INDEX products_shop_type_idx ON products (shop_type);
